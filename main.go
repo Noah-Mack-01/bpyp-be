2c7e67242ffadd0b4761860ba636b17516ebd8b3
Noah-Mack-01 <noahnewmanmack@gmail.com>
2025-05-04 20:59:04 -0400

    Integrate with react native front end

--- main.go
+++ main.go
@@ -1,45 +1,33 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	"noerkrieg.com/server/wit_connection"
+	"noerkrieg.com/server/api"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"github.com/go-chi/cors"
 )
 
 func main() {
-	uri := os.Getenv("MONGODB_URI")
-	docs := "www.mongodb.com/docs/drivers/go/current/"
-	if uri == "" {
-		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
-			"See: " + docs +
-			"usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(options.Client().
-		ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/{body}", func(writer http.ResponseWriter, r *http.Request) {
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8081", "http://localhost:3000"}, // Replace with your allowed origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value for preflight request cache
+	}))
+
+	r.Get("/v1/translate/{body}", func(writer http.ResponseWriter, r *http.Request) {
 		body := []byte(chi.URLParam(r, "body"))
-		result, err := wit_connection.ProcessMessage(&body)
+		result, err := api.ProcessMessage(&body)
 		if err != nil {
 			writer.WriteHeader(422)
 			writer.Write([]byte(fmt.Sprintf("Could not issue message to NLP with message %v: %v", body, err)))
@@ -48,6 +36,36 @@ func main() {
 			writer.WriteHeader(200)
 			writer.Write(result)
 		}
+	})
+
+	r.Get("/v1/exercises", func(writer http.ResponseWriter, r *http.Request) {
+		queryParams := r.URL.Query()
+		userID := queryParams.Get("uid")     // Access the "uid" query parameter
+		exerciseId := queryParams.Get("eid") // Access the "eid" query parameter
+
+		if userID == "" || exerciseId == "" {
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("Missing uid or eid query parameter"))
+			return
+		}
+
+		// Mock exercise JSON
+		mockExercise := map[string]any{
+			"key":             exerciseId,
+			"exercise":        "Bench Press",
+			"summary":         "5 sets of 5 reps with 185lbs",
+			"type":            "anaerobic",
+			"sets":            5,
+			"work":            5,
+			"workUnit":        "repetitions",
+			"resistance":      185,
+			"resistanceUnits": "pounds",
+			"timeStamp":       "2025-05-03T12:00:00Z",
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		json.NewEncoder(writer).Encode(mockExercise)
 	})
 	http.ListenAndServe(":3000", r)
 }
---
+++ repository/supabase.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"log"
+	"os"
+
+	"github.com/supabase-community/supabase-go"
+)
+
+func getClient() *supabase.Client {
+	client, err := supabase.NewClient(os.Getenv("BPYP_SUPABASE_URL"), os.Getenv("BPYP_SUPABASE_ANON_KEY"), &supabase.ClientOptions{})
+	if err != nil {
+		log.Fatalf("Cannot initialize client, %v", err)
+	}
+	return client
+}
+
+func test() {
+	client := getClient()
+	client.From("users").Select("*", "exact", false).Execute()
+	// data, count, err := client.From("countries").Select("*", "exact", false).Execute()
+}
+
+func GetUser(session string) (string, error) {
+	response, _, err := getClient().From("sessions").Select("user_id", "exact", false).Eq("session_id", session).Single().Execute()
+	if err != nil {
+		return "", err
+	}
+	userID := string(response)
+	return userID, nil
+}
